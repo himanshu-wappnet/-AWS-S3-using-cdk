77cb99191e9d083c51c0bcbc55c3a3a918b13abd
himanshu-wappnet <himanshu.wappnet@gmail.com>
2023-01-25 12:27:01 +0530

    changes done

--- hello_cdk/hello_cdk_stack.py
+++ hello_cdk/hello_cdk_stack.py
@@ -6,5 +6,7 @@ class HelloCdkStack(cdk.Stack):
     def __init__(self, scope: cdk.App, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        bucket = s3.Bucket(self, "MyFirstBucket", versioned=True, removal_policy=cdk.RemovalPolicy.DESTROY, auto_delete_objects=True)
+        #provide bucket name here:
+                
+        bucket = s3.Bucket(self, "BUCKET_NAME", versioned=True, removal_policy=cdk.RemovalPolicy.DESTROY, auto_delete_objects=True)
 
